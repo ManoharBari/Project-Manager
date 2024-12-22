5674ebabcd6ed76e0a18651641f94e273998c301
Manohar Kale <manoharkale5368@gmail.com>
2024-12-22 13:42:35 +0530

    22-12-2024

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb+srv://manoharkale5368:9EmOzOrAAlGHLRAg@notebook.m8rua.mongodb.net/?retryWrites=true&w=majority&appName=Notebook");
+  await mongoose.connect("mongodb+srv://manoharkale5368:Manohar2004@projectmanager.ulkve.mongodb.net/?retryWrites=true&w=majority&appName=ProjectManager");
 }
 
 module.exports = main;
